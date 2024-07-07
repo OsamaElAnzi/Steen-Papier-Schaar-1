242fd53134ad54f5ec2a0501a1aab4b4ee89974b
osamaelanzi <575808@edu.rocmn.nl>
2024-07-07 22:48:55 +0200

    nog niet klaar

--- javascript/script.js
+++ javascript/script.js
@@ -2,27 +2,55 @@ function start() {
     const speler1 = "<div><h3>Speler 1 kies uit de volgende</h3></div>";
     document.getElementById("speler1").innerHTML = speler1;
     document.getElementById("start").style.display = "none";
+
     // steen
-    const steen = `<div><button onclick="steen()">steen</button></div>`;
-    document.getElementById("steen").innerHTML = steen
+    const steen = `<button id="steenButton" onclick="steen()">steen</button>`;
+    document.getElementById("steen").innerHTML = steen;
+
     // papier
-    const papier = `<div><button onclick="papier()">papier</button></div>`;
-    document.getElementById("papier").innerHTML = papier
+    const papier = `<button id="papierButton" onclick="papier()">papier</button>`;
+    document.getElementById("papier").innerHTML = papier;
+
     // schaar
-    const schaar = `<div><button onclick="schaar()">schaar</button></div>`;
+    const schaar = `<button id="schaarButton" onclick="schaar()">schaar</button>`;
     document.getElementById("schaar").innerHTML = schaar;
+
+    document.getElementById("choices").style.display = "block";
+    document.getElementById("return").innerHTML = ''; 
 }
+
 function steen() {
-    const steen = "steen";
-    return steen;
+    const steen = "<h1>steen</h1>";
+    document.getElementById("speler1").innerHTML = steen;
+    hideChoices();
+    showReturnButton();
 }
 
 function papier() {
-    const papier = "papier";
-    return papier;
+    const papier = "<h1>papier</h1>";
+    document.getElementById("speler1").innerHTML = papier;
+    hideChoices();
+    showReturnButton();
 }
 
 function schaar() {
-    const schaar = "schaar";
-    return schaar;
+    const schaar = "<h1>schaar</h1>";
+    document.getElementById("speler1").innerHTML = schaar;
+    hideChoices();
+    showReturnButton();
+}
+
+function hideChoices() {
+    document.getElementById("choices").style.display = "none";
+}
+
+function showReturnButton() {
+    const terugNaarStart = `<button onclick="start()">terug</button>`;
+    document.getElementById("return").innerHTML = terugNaarStart;
+}
+
+function backend() {
+    if (steen()) {
+        
+    }
 }
