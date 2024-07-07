f041b3288d439f346f3a0bf8246e49f63d5b2b3a
osamaelanzi <575808@edu.rocmn.nl>
2024-07-07 23:27:31 +0200

    klaar deel 1

--- javascript/script.js
+++ javascript/script.js
@@ -4,40 +4,46 @@ function start() {
     document.getElementById("start").style.display = "none";
 
     // steen
-    const steen = `<button id="steenButton" onclick="steen()">steen</button>`;
+    const steen = `<button id="steenButton" onclick="steen()" value="steen">steen</button>`;
     document.getElementById("steen").innerHTML = steen;
 
     // papier
-    const papier = `<button id="papierButton" onclick="papier()">papier</button>`;
+    const papier = `<button id="papierButton" onclick="papier()" value="papier">papier</button>`;
     document.getElementById("papier").innerHTML = papier;
 
     // schaar
-    const schaar = `<button id="schaarButton" onclick="schaar()">schaar</button>`;
+    const schaar = `<button id="schaarButton" onclick="schaar()" value="schaar">schaar</button>`;
     document.getElementById("schaar").innerHTML = schaar;
 
     document.getElementById("choices").style.display = "block";
-    document.getElementById("return").innerHTML = ''; 
+    document.getElementById("return").innerHTML = ""; 
 }
 
 function steen() {
-    const steen = "<h1>steen</h1>";
-    document.getElementById("speler1").innerHTML = steen;
+    const steenTitle = "<h1>steen</h1>";
+    var waarde = document.getElementById("steenButton").getAttribute("value");
+    document.getElementById("speler1").innerHTML = steenTitle;
     hideChoices();
     showReturnButton();
+    winnaar(waarde);
 }
 
 function papier() {
-    const papier = "<h1>papier</h1>";
-    document.getElementById("speler1").innerHTML = papier;
+    const papierTitle = "<h1>papier</h1>";
+    var waarde = document.getElementById("papierButton").getAttribute("value");
+    document.getElementById("speler1").innerHTML = papierTitle;
     hideChoices();
     showReturnButton();
+    winnaar(waarde);
 }
 
 function schaar() {
-    const schaar = "<h1>schaar</h1>";
-    document.getElementById("speler1").innerHTML = schaar;
+    const schaarTitle = "<h1>schaar</h1>";
+    var waarde = document.getElementById("schaarButton").getAttribute("value");
+    document.getElementById("speler1").innerHTML = schaarTitle;
     hideChoices();
     showReturnButton();
+    winnaar(waarde);
 }
 
 function hideChoices() {
@@ -49,8 +55,30 @@ function showReturnButton() {
     document.getElementById("return").innerHTML = terugNaarStart;
 }
 
-function backend() {
-    if (steen()) {
-        
+function winnaar(waarde) {
+    const computer = ["steen", "papier", "schaar"];
+    var computerChoice = computer[Math.floor(Math.random() * computer.length)];
+    
+    console.log("Speler keuze: " + waarde);
+    console.log("Computer keuze: " + computerChoice);
+    
+    let resultaat = "";
+
+    if (waarde === computerChoice) {
+        resultaat = "Gelijkspel!";
+        document.getElementById("game").style.display = "none";
+    } else if (
+        (waarde === 'steen' && computerChoice === 'schaar') ||
+        (waarde === 'papier' && computerChoice === 'steen') ||
+        (waarde === 'schaar' && computerChoice === 'papier')
+    ) {
+        resultaat = "Speler wint!";
+        document.getElementById("game").style.display = "none";
+    } else {
+        resultaat = "Computer wint!";
+        document.getElementById("game").style.display = "none";
+
     }
+
+    document.getElementById("result").innerHTML = `<h2>Computer koos: ${computerChoice}</h2><h2>Jij koos :${waarde}</h2><h2>${resultaat}</h2>`;
 }
